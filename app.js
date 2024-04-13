ab3e76113dc1a1f7de380b912dfa755e63ded638
cole92 <aleksandar9212@gmail.com>
2024-04-13 10:29:36 +0200

    created a Book class and started creating a UI class.

---
+++ app.js
@@ -0,0 +1,9 @@
+// kreianje klase Book
+class Book {
+    // konstrurkorska metoda sa 3 parametra
+    constructor(tittle, author, isbn) {
+        this.tittle = tittle;
+        this.author = author;
+        this. isbn = isbn;
+    }
+}
