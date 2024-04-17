fe324b39aa7947d9163c5063df8ccd3b3081e2d1
cole92 <aleksandar9212@gmail.com>
2024-04-17 09:03:49 +0200

    Finished addBookTolist and showAlert functions of class UI

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 // kreianje klase Book
 class Book {
     // konstrurkorska metoda sa 3 parametra
-    constructor(tittle, author, isbn) {
-        this.tittle = tittle;
+    constructor(title, author, isbn) {
+        this.title = title;
         this.author = author;
         this. isbn = isbn;
     }
+}
+// kreiranje klase UI
+class UI {
+    // kreiranje metode addBookTolist klase UI
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="" class="delete">X</a></td>
+        `;
+        list.appendChild(row);
+    }
+    // kreiranje metode showAlert klase UI
+    showAlert(message, className) {
+        const div = document.createElement('div');
+        // dodajemo className
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        // dodavanje alerta
+        container.insertBefore(div, form);
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        }, 3000);
+    };
+
 }
